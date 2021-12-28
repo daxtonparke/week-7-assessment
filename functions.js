e14d9be8422f205bdb135f1ba0c4cce999d4cb73
daxtonparke <daxtonparke@gmail.com>
2021-12-27 21:03:58 -0700

    2-2

--- functions.js
+++ functions.js
@@ -1,6 +1,4 @@
 const addToZero = (arr) =>{
-// let arr = [1,2,3,4,5]
-    let answer = false
     for (let i=0; i<arr.length; i++){
     for (let j=0; j<arr.length; j++){
         console.log(arr[i],arr[j])
@@ -10,3 +8,18 @@ const addToZero = (arr) =>{
     return false
 }
 console.log(addToZero([1,-1]))
+
+
+const hasUniqueChars = (string) =>{
+let spreadString = [... new Set(string)]
+    let splitString = string.split('')
+    console.log(spreadString,splitString)
+    for(let i=0; i<string.length; i++){
+    if (spreadString[i] !== splitString[i]){
+        return false
+    }
+}
+    return true
+}
+
+console.log(hasUniqueChars('moo'))
