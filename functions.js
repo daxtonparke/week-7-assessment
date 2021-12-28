fbd71cb56b50b58c20f04c5c223c5733ca3d074d
daxtonparke <daxtonparke@gmail.com>
2021-12-27 21:27:37 -0700

    2-2 2.0

--- functions.js
+++ functions.js
@@ -7,19 +7,18 @@ const addToZero = (arr) =>{
     }}
     return false
 }
-console.log(addToZero([1,-1]))
+// console.log(addToZero([1,-1]))
 
 
 const hasUniqueChars = (string) =>{
-let spreadString = [... new Set(string)]
+    let spreadString = [... new Set(string)]
     let splitString = string.split('')
-    console.log(spreadString,splitString)
-    for(let i=0; i<string.length; i++){
-    if (spreadString[i] !== splitString[i]){
-        return false
-    }
-}
-    return true
+    let spreadWord = spreadString.join('')
+    let splitWord = splitString.join('')
+    console.log(spreadWord,splitWord)
+if(splitWord === spreadWord){
+    return true}
+    return false
 }
 
-console.log(hasUniqueChars('moo'))
+console.log(hasUniqueChars('neva'))
